2e24773fbf2b0129343c29937c5c0d37eb0fd809
Vagarshak Kosyan <vag.kosyan@gmail.com>
2024-09-24 16:59:24 +0400

    Сохранение изменений в App.jsx перед слиянием

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Logistic from './components/Logistic/Logistic'
-import "./App.scss"
-import About from './components/About/About'
-import Service from './components/Service/Service'
+import Logistic from "./components/Logistic/Logistic";
+import "./App.scss";
+import About from "./components/About/About";
+import Service from "./components/Service/Service";
 
 export default function App() {
   return (
     <div>
-      <Logistic/>
-      <About/>
-      <Service/>
+      <Logistic />
+      <About />
+      <Service />
     </div>
-  )
+  );
 }
